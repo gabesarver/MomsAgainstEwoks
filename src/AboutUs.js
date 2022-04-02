f82e89d6c6e910558d3b13be3b7fa6ada2f0c9dd
gabesarver <gabesarver@DESKTOP-9JRDV71.localdomain>
2022-04-02 17:07:02 -0400

    About us page change

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,41 +1,47 @@
 import React from 'react'
 import './TextStyles.css'
 import EwokNavbar from './EwokNavbar.js'
+import background from './neongrid.png'
 //import './components/TextStyles.'
 
 
 function AboutUs() {
     return (
-        <div>
-            <EwokNavbar />
+        <body className='body'>
+            <div style={{ backgroundImage: `url(${background})`, 
+                backgroundRepeat: 'repeat',
+            }}>
+                <EwokNavbar />
             
 
-            <text className='header1'> About Us </text><br></br>
-            <text className='body'>
-            We are Moms Against Ewoks (MAE), and our mission is to 
-            protect the world’s children from the satanic man-eating 
-            creatures marketed to them. We hope that by raising 
-            awareness and informing the public about this issue we 
-            can enact change in retailers nationwide, and end the 
-            efforts to indoctrinate our children through the selling 
-            of Ewok toys.<br></br>
-            </text>
+                <center className='header1'> About Us </center><br></br>
+                <text className='body'>
+                We are Moms Against Ewoks (MAE), and our mission is to 
+                protect the world’s children from the satanic man-eating 
+                creatures marketed to them. We hope that by raising 
+                awareness and informing the public about this issue we 
+                can enact change in retailers nationwide, and end the 
+                efforts to indoctrinate our children through the selling 
+                of Ewok toys.<br></br>
+                </text>
 
-            <text className='header1'>What are Ewoks?<br></br></text>
-            <text className='body'>
-            These furry friends of Mephistopheles appear in the new 
-            movie Star Wars: Return of the Jedi, and help the “heroes”
-             of the movie commit violent acts of terrorism with the 
-             aid of Satanic forces. They are short creatures with 
-             dog-like noses to remind children of their loyal furry 
-             friends, human-looking faces to trick our kids into 
-             seeing them as being created in God’s image, and a teddy
-              bear anatomy complimented with beady eyes to lure our
-             children into the embrace of Lucifer. 
-            </text>
+                <center className='header1'>What are Ewoks?<br></br></center>
+                <text className='body'>
+                 These furry friends of Mephistopheles appear in the new 
+                 movie Star Wars: Return of the Jedi, and help the “heroes”
+                 of the movie commit violent acts of terrorism with the 
+                 aid of Satanic forces. They are short creatures with 
+                 dog-like noses to remind children of their loyal furry 
+                 friends, human-looking faces to trick our kids into 
+                 seeing them as being created in God’s image, and a teddy
+                  bear anatomy complimented with beady eyes to lure our
+                 children into the embrace of Lucifer. 
+                </text>
 
 
-        </div>
+            </div>
+        </body>
+        
     );
 
 
--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
       </div>
       <div className="App">
-        WELCOME TO HELL
+        
       </div>
       <EwokNavbar />
 
