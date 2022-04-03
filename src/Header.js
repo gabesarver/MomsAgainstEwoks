a3afc1a90641ece0af3205bb2dedab86a2511ec1
gabesarver <gabesarver@DESKTOP-9JRDV71.localdomain>
2022-04-02 20:46:20 -0400

    Added Date to top left

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import logo from './images/fuckewok.svg';
 import './header.css';
+import './TextStyles.css';
 
 export default function Header() {
   return (
@@ -9,7 +10,9 @@ export default function Header() {
             <img src={logo} className="App-logo" alt="logo" />
         </div>
         <div className='background'>
-            
+            <div className='date'>
+              <text className="body"> June 25, 1983 </text>
+            </div>
         </div>
     </div>
   )
